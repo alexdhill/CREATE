3745e822b0a15b99996ed2af127457e4abe97354
Alex Hill <ahill6@ucsc.edu>
2026-05-14 10:01:28 -0700

    Patching annotation parser for IsoQuant

--- bin/python/correct_isoquant.py
+++ bin/python/correct_isoquant.py
@@ -1,11 +1,35 @@
+def attrs_to_dict(attrs):
+    '''Extract attributes from GTF/GFF3 annotation.'''
+    attr_dict = {}
+    for attr in attrs.split(';'):
+        if attr.strip() == '':
+            continue
+        key, value = attr.strip().split(' ')
+        attr_dict[key] = value.strip('"')
+    return attr_dict
+
 def read_ann(path):
+    '''Add gene_name + gene_type to IsoQuant GTF/GFF3 transcript annotations.'''
     with open(path, 'r') as f:
-        for line in f:
-            transcript_id = [attr.split('"')[1].strip() for attr in line.strip().split('\t')[-1].split(';') if attr.strip().startswith("transcript_id")][0]
-            if '.nnic' == transcript_id[-5:]:
-                gene_id = [attr.split('"')[1].strip() for attr in line.strip().split('\t')[-1].split(';') if attr.strip().startswith("gene_id")][0]
-                line = line.strip() + " gene_name \"{}\"; gene_type \"novel\";".format(gene_id)
-            yield line.strip()
+        curr_gene_info = {}
+        for ann in f:
+            if ann.startswith('#'):
+                yield ann.strip()
+                continue
+            if ann.strip().split('\t')[2] == 'gene':
+                curr_gene_info = attrs_to_dict(ann.strip().split('\t')[-1])
+                if (not curr_gene_info.get('gene_name')): 
+                    gene_id = curr_gene_info.get('gene_id', '')
+                    gene_name = gene_id.replace('novel_gene_', 'Novel gene ')
+                    curr_gene_info['gene_name'] = gene_name
+                    curr_gene_info['gene_biotype'] = 'Novel gene'
+                    yield ann.strip() + ' gene_name \"{}\"; gene_biotype \"Novel gene\"'.format(gene_name)
+                yield ann.strip()
+                continue
+            if ann.strip().split('\t')[2] == 'transcript':
+                yield ann.strip() + ' gene_name \"{}\"; gene_biotype \"{}\"'.format(curr_gene_info.get('gene_name', ''), curr_gene_info.get('gene_biotype', ''))
+                continue
+            yield ann.strip()
 
 def main():
     from sys import argv
